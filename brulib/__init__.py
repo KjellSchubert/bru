167c811c3ecdd98569c2a8c2122fe89ac5a53ca4
kjellschubert <kjell.schubert@gmail.com>
2015-01-01 20:49:21 +0000

    does python <= 3.2 insist on __init__.py?

---
+++ brulib/__init__.py
@@ -0,0 +1,4 @@
+# see http://python-notes.curiousefficiency.org/en/latest/python_concepts/import_traps.html
+# for 'missing __init__.py trap'. I suspect this is what causes bru-sample 
+# builds to fail on travis-ci with language: cpp and while apt-get installs 
+# python 3.2
