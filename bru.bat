d9c70cd2bd6017c2482decf14e84085e13707459
Kjell Schubert <kjell.schubert@gmail.com>
2015-05-22 12:17:28 -0700

    temp hacks to build with msvs 2015

--- bru.bat
+++ bru.bat
@@ -2,5 +2,5 @@
 rem This is the equivalent of bru.sh on Windows.
 rem Add the parent dir of bru.bat to your PATH.
 SET script_dir=%~dp0
-call python3 %script_dir%/autoupdate.py --hours 24
-call python3 %script_dir%/bru.py %*
+call python %script_dir%/autoupdate.py --hours 24
+call python %script_dir%/bru.py %*
